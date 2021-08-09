900ef13524ec4939be3b767451d1ab0f5fa0b6c2
Kyle Schechter <kyle.schechter@powerhrg.com>
2021-08-09 10:07:26 -0400

    done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  def match array
+    check = array.filter {|str| str.chars.sort == @word.chars.sort}
+    check.size > 0 ? check : []
+  end
+end
